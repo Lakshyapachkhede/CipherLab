b4c2eda3492f9ba9af981227e34953f272a3a89d
lakshya pachkhede <pachkhedelakshya@gmail.com>
2025-09-07 10:01:03 +0530

    added support for polyalphabetic cipher and started working on playfair

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ const navLinks = document.querySelector(".nav-links");
 
 
 const alphabets = "abcdefghijklmnopqrstuvwxyz";
+const capitalAlphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 const algorithms = {
     "1": { name: "ceaser", encrypt: ceaserEncrypt, decrypt: ceaserDecrypt },
     "2": { name: "monoalphabetic", encrypt: monoalphabeticEncrypt, decrypt: monoalphabeticDecrypt },
-    "3": { name: "playfair" },
-    "4": { name: "polyalphabetic" },
+    "3": { name: "playfair", encrypt: playfairEncrypt, decrypt: playfairDecrypt },
+    "4": { name: "polyalphabetic(Vigenère)", encrypt: polyalphabeticEncrypt, decrypt: polyalphabeticDecrypt },
     "5": { name: "rail fence" },
     "6": { name: "columnar" }
 }
@@ -59,11 +60,11 @@ function shiftChar(char, key) {
 
 
 function isDigitsOnly(str) {
-  return /^\d+$/.test(str);
+    return /^\d+$/.test(str);
 }
 
 function getKeyByValue(obj, value) {
-  return Object.keys(obj).find(key => obj[key] === value);
+    return Object.keys(obj).find(key => obj[key] === value);
 }
 
 // ################################# UTILITY FUNCTION ENDS ########################################
@@ -72,10 +73,10 @@ function getKeyByValue(obj, value) {
 
 // ################################# CEASER CIPHER ########################################
 
-function getCeaserKey(){
+function getCeaserKey() {
     let key = keyBox.value.replace(/\s+/g, "");
 
-    if(!isDigitsOnly(key)){
+    if (!isDigitsOnly(key)) {
         showErrorBox("Enter numeric key");
         return NaN;
 
@@ -90,7 +91,7 @@ function getCeaserKey(){
         return NaN;
     }
 
-    if(key < 1 || key > 25){
+    if (key < 1 || key > 25) {
         key = ((key % 26) + 26) % 26;
     }
 
@@ -154,16 +155,16 @@ function ceaserDecrypt(encrypted) {
 // ################################# MONOALPHABETIC CIPHER ########################################
 
 
-function getMonoalphabeticCipherKey(){
+function getMonoalphabeticCipherKey() {
     let key = keyBox.value.replace(/\s+/g, "");
 
-    if(!isLetter(key)){
+    if (!isLetter(key)) {
         showErrorBox("Enter alphabetic key");
         return null;
 
     }
 
-    if(key.length != 26){
+    if (key.length != 26) {
         showErrorBox("key should be of length 26");
         return null;
     }
@@ -172,11 +173,12 @@ function getMonoalphabeticCipherKey(){
 }
 
 
-function monoalphabeticCreateMap(key){
+function monoalphabeticCreateMap(key) {
     const map = {};
 
-    for(let i = 0; i < 26; i++){
-        map[alphabets[i]] = key[i];
+    for (let i = 0; i < 26; i++) {
+
+        map[alphabets[i]] = key[i].toLowerCase();
         map[alphabets[i].toUpperCase()] = key[i].toUpperCase();
     }
 
@@ -184,13 +186,13 @@ function monoalphabeticCreateMap(key){
 }
 
 
-function monoalphabeticEncrypt(message){
+function monoalphabeticEncrypt(message) {
     let encrypted = "";
 
 
     let key = getMonoalphabeticCipherKey();
-    
-    if(key == null){
+
+    if (key == null) {
         return "";
     }
 
@@ -210,13 +212,13 @@ function monoalphabeticEncrypt(message){
 
 
 
-function monoalphabeticDecrypt(encrypted){
+function monoalphabeticDecrypt(encrypted) {
     let message = "";
 
 
     let key = getMonoalphabeticCipherKey();
-    
-    if(key == null){
+
+    if (key == null) {
         return "";
     }
 
@@ -236,6 +238,195 @@ function monoalphabeticDecrypt(encrypted){
 }
 
 // ################################# MONOALPHABETIC CIPHER ENDS ########################################
+
+
+
+
+// ################################# POLYALPHABETIC CIPHER ########################################
+
+function getPolyalphabeticCipherKey() {
+    let key = keyBox.value.replace(/\s+/g, "");
+
+    if (!isLetter(key)) {
+        showErrorBox("Enter alphabetic key");
+        return null;
+    }
+
+    return key;
+}
+
+function polyalphabeticEncrypt(message) {
+    let encrypted = "";
+
+
+    let key = getPolyalphabeticCipherKey();
+
+    if (key == null) {
+        return "";
+    }
+
+    let keyIndex = 0;
+
+
+    for (let i = 0; i < message.length; i++) {
+
+        if (isLetter(message[i])) {
+
+            let messageChar = message[i];
+            let keyChar = key[keyIndex];
+
+            const base = messageChar === messageChar.toUpperCase() ? 65 : 97;
+            const baseKey = keyChar === keyChar.toUpperCase() ? 65 : 97;
+
+            let charCode = messageChar.charCodeAt(0) - base;
+            let keyCode = keyChar.charCodeAt(0) - baseKey;
+
+            encrypted += shiftChar(messageChar, keyCode);
+
+        } else {
+            encrypted += message[i];
+        }
+
+
+
+
+        keyIndex = (keyIndex + 1) % key.length;
+
+    }
+
+    return encrypted;
+
+}
+
+function polyalphabeticDecrypt(encrypted) {
+    let message = "";
+
+
+    let key = getPolyalphabeticCipherKey();
+
+    if (key == null) {
+        return "";
+    }
+
+    let keyIndex = 0;
+
+
+    for (let i = 0; i < encrypted.length; i++) {
+        if (isLetter(encrypted[i])) {
+            let encryptedChar = encrypted[i];
+            let keyChar = key[keyIndex];
+
+            const base = encryptedChar === encryptedChar.toUpperCase() ? 65 : 97;
+            const baseKey = keyChar === keyChar.toUpperCase() ? 65 : 97;
+
+            let charCode = encryptedChar.charCodeAt(0) - base;
+            let keyCode = keyChar.charCodeAt(0) - baseKey;
+
+            message += shiftChar(encryptedChar, -keyCode);
+        } else {
+            message += encrypted[i];
+        }
+        keyIndex = (keyIndex + 1) % key.length;
+
+    }
+
+    return message;
+}
+
+
+
+// ################################# POLYALPHABETIC CIPHER ENDS ########################################
+
+
+// ################################# PLAYFAIR CIPHER ########################################
+
+function getPlayfairCipherKey() {
+    return getPolyalphabeticCipherKey(); // same logic for both
+}
+
+
+function getPlayfairGrid(key) {
+    const rows = 5;
+    const cols = 5;
+    const charGrid = Array.from({ length: rows }, () => Array(cols).fill(' '));
+
+    const charSet = new Set();
+
+
+
+    for (let char of key) {
+        if(char == 'j'){
+            continue;
+        }
+        if (!charSet.has(char)) {
+            charSet.add(char)
+        }
+    }
+
+    for(let char of alphabets){
+        if(char == 'j'){
+            continue;
+        }
+        if (!charSet.has(char)) {
+            charSet.add(char)
+        }
+    }
+
+    let index = 0;
+
+    for(let char of charSet){
+        let i = Math.floor(index / rows);
+        let j = index % cols ;
+
+        charGrid[i][j] = char;
+
+        index++;
+
+    }
+    
+    return charGrid;
+}
+
+function playfairEncrypt(message) {
+    let encrypted = "";
+
+
+    let key = getPlayfairCipherKey();
+
+    if (key == null) {
+        return "";
+    }
+
+    const charGrid = getPlayfairGrid(key);
+
+    
+
+
+
+
+
+
+
+
+
+}
+
+
+
+function playfairDecrypt(encrypted) {
+
+}
+
+
+
+
+
+
+
+// ################################# PLAYFAIR CIPHER ENDS ########################################
+
+
+
 
 // ################################# MAIN HANDLERS ########################################
 
@@ -264,7 +455,31 @@ cipherBox.addEventListener("input", function (e) {
     }
 });
 
+keyBox.addEventListener("input", function (e) {
+    if (checkBeforeEncryptionOrDecryption()) {
 
+        if (messageBox.value != '') {
+            cipherBox.value = algorithms[algorithmSelect.value].encrypt(messageBox.value);
+        } else {
+            messageBox.value = algorithms[algorithmSelect.value].decrypt(cipherBox.value);
+        }
+
+
+    }
+});
+
+algorithmSelect.addEventListener("change", function (e) {
+    if (checkBeforeEncryptionOrDecryption()) {
+
+        if (messageBox.value != '') {
+            cipherBox.value = algorithms[algorithmSelect.value].encrypt(messageBox.value);
+        } else {
+            messageBox.value = algorithms[algorithmSelect.value].decrypt(cipherBox.value);
+        }
+
+
+    }
+});
 
 hamburger.addEventListener("click", (e) => {
     e.stopPropagation(); // Prevent the click from reaching document
